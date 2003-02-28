1b25d3698f672c7803dc69981e687638920733bc
dready <dready>
2003-02-28 15:41:26 +0000

    updated box creation to get rid of incode HTML

--- diskotek/modules/box/box_whatsnew.php
+++ diskotek/modules/box/box_whatsnew.php
@@ -10,12 +10,16 @@ function dok_box_whatsnew($display_module, $theme_path) {
 	$t = new template ($theme_path);
 	$t->set_file('page','box_default.tpl');
 	$t->set_var('BOXTITLE',MSG_TITLE_BOX_LAST_SONG);
-	$content = '';
-	while ( $row = mysql_fetch_array($res) ) {
-		$content .= '<a class="boxlink" href="'.$_SERVER['PHP_SELF'].'?display=view_song&id='.$row['id'].'">
-		<div class="boxlink">'.$row['name'].'</div></a>';
+	$t->set_block('page','boxlink','boxlinktag');
+	$t->set_var('boxlinktag','');
+	if ( !mysql_numrows($res) ) {
+		$t->set_var('BOXCONTENT','');
 	}
-	$t->set_var('BOXCONTENT',$content);
+	while ( $row = mysql_fetch_array($res) ) {
+		$t->set_var('LINK',$_SERVER['PHP_SELF'].'?display=view_song&id='.$row['id']);
+		$t->set_var('LABEL',$row['name']);
+		$t->parse('BOXCONTENT','boxlink','true');
+	}
 	return $t->parse('out','page');
 }
 
--- diskotek/themes/default/box_default.tpl
+++ diskotek/themes/default/box_default.tpl
@@ -2,3 +2,7 @@
 <div class="boxtitle">{BOXTITLE}</div>
 <div class="boxcontent">{BOXCONTENT}</div>
 </div>
+<!-- BEGIN boxlink -->
+<a class="boxlink" href="{LINK}">
+<div class="boxlink">{LABEL}</div></a>
+<!-- END boxlink -->
